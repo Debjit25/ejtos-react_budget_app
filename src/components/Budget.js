c1baa7f67232482fb85444034447f4a90fa08bb7
Debjit Maji <debjitmaji123@gmail.com>
2024-07-01 15:24:44 -0400

    Latest commit

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,18 +1,50 @@
-
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses, currency } = useContext(AppContext);
+    const totalSpent = expenses.reduce((total, item) => {
+        return total + item.cost;
+    }, 0);
     const [newBudget, setNewBudget] = useState(budget);
+    const [error, setError] = useState('');
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-    }
+        const value = parseInt(event.target.value, 10);
+        if (isNaN(value) || value <= 0) {
+            setNewBudget('');
+            setError('Please enter a valid budget.');
+            return;
+        }
+
+        setNewBudget(value);
+        if (value > 20000) {
+            setError('Budget cannot exceed 20,000');
+        } else if (value < totalSpent) {
+            setError(`Budget cannot be lower than the total spending of ${currency}${totalSpent}`);
+        } else {
+            setError('');
+        }
+    };
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span>Budget: {currency}{budget}</span>
+            <div className="input-group">
+                <div className="input-group-prepend">
+                    <span className="input-group-text">{currency}</span>
+                </div>
+                <input
+                    type="number"
+                    step="10"
+                    value={newBudget}
+                    onChange={handleBudgetChange}
+                    className="form-control"
+                />
+            </div>
+            {error && <div className="text-danger">{error}</div>}
+        </div>
     );
 };
+
 export default Budget;
